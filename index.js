e30e95a576410e51585a1f3bbaa723188301b1a9
Yu Han <yuhan28283@gmail.com>
2022-05-01 13:11:16 -0400

    updating

--- index.js
+++ index.js
@@ -11,16 +11,15 @@ function init() {
     .prompt([
       {
         type: "input",
-        name: "title",
+        name: "role",
         message: "What is your role?",
-        choices: ["Manager", "Engineer", "Intern"],
       },
     ])
     .then(function (answers) {
       console.log(answers);
       console.log(answers.role);
       if (answers.role == "Manager") {
-       return inquirer
+        return inquirer
           .prompt([
             {
               type: "input",
@@ -40,65 +39,93 @@ function init() {
           ])
           .then(function (ManagerAnswers) {
             console.log(ManagerAnswers);
-            console.log(ManagerAnswers.id, ManagerAnswers.email, ManagerAnswers.number);
-          }); 
-        }
-          else if(answers.role == "Engineer") {
-          return inquirer
-            .prompt([
-                {
-                    type: "input",
-                    name: "id",
-                    message: "What is your ID?",
-                  },
-                  {
-                    type: "input",
-                    name: "email",
-                    message: "What is your Email?",
-                  },
-                  {
-                    type: "input",
-                    name: "github",
-                    message: "what is your GitHub username?",
-                  },
-            ])
-            .then(function (EngineerAnswers) {
-                console.log(EngineerAnswers);
-                console.log(EngineerAnswers.id, EngineerAnswers.email, EngineerAnswers.github);
-              });
-            }
-            else(answers.role == "Intern"); {
-               return inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        name: "title",
-                        message: "What is your role?",
-                        choices:["Manager", "Engineer", "Intern"]
-                    },
-                    {
-                        type: "input",
-                        name: "id",
-                        message: "What is your ID?",
-                      },
-                      {
-                        type: "input",
-                        name: "email",
-                        message: "What is your Email?",
-                      },
-                      {
-                        type: "input",
-                        name: "school",
-                        message: "Which school did you attend?",
-                      },
-                ])
-                .then(function (InternAnswers) {
-                    console.log(InternAnswers);
-                    console.log(InternAnswers.id, InternAnswers.email, InternAnswers.school);
-                  });
-                }
-                });
+            console.log(
+              ManagerAnswers.id,
+              ManagerAnswers.email,
+              ManagerAnswers.number
+            );
+          });
+      } else if (answers.role == "Engineer") {
+        return inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "id",
+              message: "What is your ID?",
+            },
+            {
+              type: "input",
+              name: "email",
+              message: "What is your Email?",
+            },
+            {
+              type: "input",
+              name: "github",
+              message: "what is your GitHub username?",
+            },
+          ])
+          .then(function (EngineerAnswers) {
+            console.log(EngineerAnswers);
+            console.log(
+              EngineerAnswers.id,
+              EngineerAnswers.email,
+              EngineerAnswers.github
+            );
+          });
+      } else if (answers.role == "Intern");
+      {
+        return inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "id",
+              message: "What is your ID?",
+            },
+            {
+              type: "input",
+              name: "email",
+              message: "What is your Email?",
+            },
+            {
+              type: "input",
+              name: "school",
+              message: "Which school did you attend?",
+            },
+          ])
+          .then(function (InternAnswers) {
+            console.log(InternAnswers);
+            console.log(
+              InternAnswers.id,
+              InternAnswers.email,
+              InternAnswers.school
+            );
+          });
+      }
+    });
 }
+function addMember() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'memberChoice',
+          message: 'Which type of team member would you like to add?',
+          choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+            "No more team members",
+          ],
+        },
+      ])
+      .then((userChoice) => {
+        if (answers.title == "Manager") {
+          // do manager stuff
+        }
+      });
+  }
 
 // Function call to initialize app
 init();
+
+
